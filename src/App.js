a832379f22d7e6e5e4bc0d98cdbe6c8d7efdc653
Raveena10 <65342778+Raveena10@users.noreply.github.com>
2022-12-25 18:34:19 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          External Exam Devops
+        </h1>
+    </div>
+  );
+}
+
+export default App;
